18eb35b21b89a6abcfc252a73e070d083ad95e2d
Maksim <maxgrank88@gmail.com>
2026-03-22 19:06:09 +0300

    setup dagger hilt for dependency injection

--- app/src/main/java/com/max_grank/myapplication/di/MainModule.kt
+++ app/src/main/java/com/max_grank/myapplication/di/MainModule.kt
@@ -3,6 +3,13 @@ package com.max_grank.myapplication.di
 import android.app.Application
 import androidx.room.Room
 import com.max_grank.myapplication.data.AppDataBase
+import com.max_grank.myapplication.data.add_item.AddItemRepository
+import com.max_grank.myapplication.data.add_item.AddItemRepositoryImpl
+import com.max_grank.myapplication.data.note_item.NoteIItemRepositoryImpl
+import com.max_grank.myapplication.data.note_item.NoteItem
+import com.max_grank.myapplication.data.note_item.NoteItemRepository
+import com.max_grank.myapplication.data.shoppinglist_item.ShoppingListRepository
+import com.max_grank.myapplication.data.shoppinglist_item.ShoppingListRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,5 +28,23 @@ object MainModule {
             AppDataBase::class.java,
             "shopping_list"
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAddItemRepository(appDataBase: AppDataBase): AddItemRepository {
+        return AddItemRepositoryImpl(appDataBase.addItemDao())
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteItemRepository(appDataBase: AppDataBase): NoteItemRepository {
+        return NoteIItemRepositoryImpl(appDataBase.noteItemDao())
+    }
+
+    @Provides
+    @Singleton
+    fun provideShoppingListItemRepository(appDataBase: AppDataBase): ShoppingListRepository {
+        return ShoppingListRepositoryImpl(appDataBase.shoppingListDao())
     }
 }
